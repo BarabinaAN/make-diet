897416074dac07106bb7d3b360bc37be08274203
Natalya Barabina <barabash4ka@gmail.com>
2020-09-02 19:01:44 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './style.scss'
+
+const App = () => <h1>App component is include</h1>
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env={}) => {
+
+  const { mode='dev' } = env
+  const isProd = mode === 'prod' 
+  const isDev = mode === 'dev' 
+
+  const getStyleLoaders = () => {
+    return [
+      isProd ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader'
+    ]
+  }
+
+  const getPlugins = () => {
+    let plugins = [
+      new HtmlWebpackPlugin({
+        template: 'public/index.html'
+      })
+    ]
+    if(isProd) {
+      plugins.push(
+        new MiniCssExtractPlugin({
+          filename: '[name]-[hash:7].css'
+        })
+      )
+    }
+    return plugins
+  }
+
+  return {
+    mode: isProd ? 'production' : isDev && "development",
+    module: {
+      rules: [
+        {
+          test: /\.js/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.(jpg|svg|png|jpeg|ico)$/,
+          use: [{
+            loader: 'file-loader',
+            options: {
+              outpath: 'images',
+              name: '[name]-[sha1:hash:7].[ext]'
+            }
+          }]
+        },
+        {
+          test: /\.css/,
+          use: getStyleLoaders()
+        },
+        {
+          test: /\.s[ca]ss/,
+          use: [...getStyleLoaders(), 'sass-loader']
+        }
+      ]
+    },
+    plugins: getPlugins(),
+    devServer: {
+      open: true
+    }
+  }
+}
