5894f4462ff416ac929976e2632811a482e90a0d
Natalya Barabina <barabash4ka@gmail.com>
2020-09-05 13:18:50 +0300

    include error-boundry

--- src/components/error-boundry/error-boundry.js
+++ src/components/error-boundry/error-boundry.js
@@ -1,9 +1,19 @@
 import React, {Component} from 'react'
+import ErrorIndicator from '../error-indicator'
 
 export default class ErrorBoundry extends Component {
+  state={
+    hasError: false
+  }
+
+  componentDidCatch() {
+    this.setState({ hasError: true })
+  }
+
   render() {
-    return (
-      <p>ErrorBoundry</p>
-    )
+    const {hasError} = this.state
+    const {children} = this.props
+    const content = hasError ? <ErrorIndicator /> : children
+    return content
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import ReactDOM from 'react-dom'
 import App from './components/app'
 import TestService from './services/test-service'
 import { ServiceProvider } from './components/service-context'
+import ErrorBoundry from './components/error-boundry'
+
 import './scss/index.scss'
 
 const service = new TestService()
 
 ReactDOM.render(
-  <ServiceProvider value={service}>
-    <App />
-  </ServiceProvider>,
+  <ErrorBoundry>
+    <ServiceProvider value={service}>
+      <App />
+    </ServiceProvider>
+  </ErrorBoundry>,
   document.getElementById('root')
 )
